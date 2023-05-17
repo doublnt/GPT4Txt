fc2be79717f56a1927f6201ae4180d27693407c6
yinxi <yinximail@gmail.com>
2023-05-17 22:04:03 +0800

    intergrate with langchain agent

--- langchainmodel.py
+++ langchainmodel.py
@@ -2,29 +2,142 @@ from langchain.llms import LlamaCpp
 from langchain import PromptTemplate, LLMChain
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.prompts import StringPromptTemplate
+from langchain.agents import Tool, LLMSingleActionAgent, AgentOutputParser,AgentExecutor
+from langchain.schema import AgentAction, AgentFinish
+
+from typing import List, Union
+import re
+
+# Set up a prompt template
 
 
-def main():
-    template = """Question: {question}
+class CustomPromptTemplate(StringPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
 
-    Answer: Let's think step by step."""
+    def format(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join(
+            [f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        return self.template.format(**kwargs)
 
-    prompt = PromptTemplate(template=template, input_variables=["question"])
 
-    local_path = "../models/ggml-model-q8_0.bin"
+class CustomOutputParser(AgentOutputParser):
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split(
+                    "Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
 
-    # Make sure the model path is correct for your system!
-    # Callbacks support token-wise streaming
-    # Verbose is required to pass to the callback manager
 
+def create_llm():
+    local_path = "..\\models\\ggml-model-q8_0.bin"
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     llm = LlamaCpp(
-        model_path=local_path, callback_manager=callback_manager, verbose=True
+        model_path=local_path, callback_manager=callback_manager, verbose=True, n_ctx=4096
     )
+
+    return llm
+
+
+def llm_chain():
+    template = """Question: {question}
+    Answer: Let's think step by step."""
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm = create_llm()
 
     llm_chain = LLMChain(prompt=prompt, llm=llm)
     question = "who is su shi first wife?"
     llm_chain.run(question)
+
+
+def agent():
+    llm = create_llm()
+    search_tool = DuckDuckGoSearchRun()
+    tools = [
+        Tool.from_function(
+            func=search_tool.run,
+            name="Search",
+            description="useful for when you need to answer questions about current events",
+            return_direct=True
+            # coroutine= ... <- you can specify an async method if desired as well
+        ),
+    ]
+
+    # Set up the base template
+    template = """Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin! Remember to speak as a pirate when giving your final answer. Use lots of "Arg"s
+
+    Question: {input}
+    {agent_scratchpad}"""
+
+    prompt = CustomPromptTemplate(
+        template=template,
+        tools=tools,
+        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+        # This includes the `intermediate_steps` variable because that is needed
+        input_variables=["input", "intermediate_steps"]
+    )
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    tool_names = [tool.name for tool in tools]
+    output_parser = CustomOutputParser()
+
+    agent = LLMSingleActionAgent(
+        llm_chain=llm_chain,
+        output_parser=output_parser,
+        stop=["\nObservation:"],
+        allowed_tools=tool_names
+    )
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+    agent_executor.run("How many people live in canada as of 2023?")
+
+def main():
+    # llm_chain()
+    agent()
 
 
 if __name__ == "__main__":
